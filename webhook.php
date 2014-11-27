dad72e5e6041dc29baed089c3528253ab8456551
Miles Johnson <mileswjohnson@gmail.com>
2014-11-26 16:26:38 -0800

    Use getenv() for fetching the token.

--- webhook.php
+++ webhook.php
@@ -12,17 +12,15 @@ try {
         throw new Exception('No payload detected.', 400);
     }
 
-    if (empty($_SERVER['HTTP_X_HUB_SIGNATURE']) || empty($_ENV['SECRET_TOKEN'])) {
+    if (empty($_SERVER['HTTP_X_HUB_SIGNATURE']) || !getenv('SECRET_TOKEN')) {
         throw new Exception('Missing secret token.', 400);
     }
 
-    if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== 'sha1=' . hash_hmac('sha1', $_POST['payload'], $_ENV['SECRET_TOKEN'], false)) {
+    if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== 'sha1=' . hash_hmac('sha1', $_POST['payload'], getenv('SECRET_TOKEN'), false)) {
         throw new Exception('Failed to validate secret token.', 401);
     }
 
     // Update the repository
-    $payload = json_decode($_POST['payload']);
-
     http_response_code(200);
     echo shell_exec(sprintf('cd %s && git reset --hard HEAD && git pull', __DIR__));
 
