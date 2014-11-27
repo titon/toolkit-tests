c72d11d3eccd8d42dc35b0caff6784ba635e4a40
Miles Johnson <mileswjohnson@gmail.com>
2014-11-26 16:39:34 -0800

    Use input since $_POST wasn't working.

--- webhook.php
+++ webhook.php
@@ -8,20 +8,26 @@ try {
         throw new Exception('Missing X-GitHub-Event header.', 400);
     }
 
-    if (empty($_POST['payload'])) {
+    $payload = file_get_contents('php://input');
+    $token = getenv('SECRET_TOKEN');
+
+    if (empty($payload)) {
         throw new Exception('No payload detected.', 400);
     }
 
-    if (empty($_SERVER['HTTP_X_HUB_SIGNATURE']) || !getenv('SECRET_TOKEN')) {
+    if (empty($_SERVER['HTTP_X_HUB_SIGNATURE']) || !$token) {
         throw new Exception('Missing secret token.', 400);
     }
 
-    if ($_SERVER['HTTP_X_HUB_SIGNATURE'] !== 'sha1=' . hash_hmac('sha1', $_POST['payload'], getenv('SECRET_TOKEN'), false)) {
+    list($algorithm, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE']);
+
+    if ($hash !== hash_hmac($algorithm, $payload, $token, false)) {
         throw new Exception('Failed to validate secret token.', 401);
     }
 
     // Update the repository
     http_response_code(200);
+    echo 'Updating git repository...' . PHP_EOL;
     echo shell_exec(sprintf('cd %s && git reset --hard HEAD && git pull', __DIR__));
 
 } catch (Exception $e) {
